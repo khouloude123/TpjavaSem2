19cfdb17b8d572b5044106d003f6e55c3ec53d76
khouloude123 <khouloudefakraoui1@gmail.com>
2025-02-28 11:13:24 +0100

    String Buffer

--- TP8_chaine/exercice2.java
+++ TP8_chaine/exercice2.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class exercice2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String mot1, mot2;
+        StringBuffer text;
         
         System.out.print("Entrez une phrase (max 50 caractères) : ");
-        String ch = scanner.nextLine();
+        text = new StringBuffer(scanner.nextLine());
         
-        if (ch.length() > 50) {
+        if (text.length() > 50) {
             System.out.println("Erreur : La phrase ne doit pas dépasser 50 caractères.");
             return;
         }
         
 
         System.out.print("Entrez le mot à remplacer (max 10 caractères) : ");
-        String mot1 = scanner.nextLine();
+        mot1 = scanner.nextLine();
         
         if (mot1.length() > 10) {
             System.out.println("Erreur : Le mot à remplacer ne doit pas dépasser 10 caractères.");
@@ -23,17 +25,17 @@ public class exercice2 {
         
     
         System.out.print("Entrez le mot de remplacement (max 10 caractères) : ");
-        String mot2 = scanner.nextLine();
+        mot2 = scanner.nextLine();
         
         if (mot2.length() > 10) {
             System.out.println("Erreur : Le mot de remplacement ne doit pas dépasser 10 caractères.");
             return;
         }
         
-        String chModifie = ch.replace(mot1, mot2);
+        text = new StringBuffer(text.toString().replace(mot1, mot2));
         
-        System.out.println("Texte modifié : " + chModifie);
+        System.out.println("Texte modifié : " + text);
         
-        scanner.close();
+
     }
 }
