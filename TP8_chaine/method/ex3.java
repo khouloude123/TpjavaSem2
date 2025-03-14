8d392e0b2cf892c79f204e177bd4d024bcc92540
khouloude123 <khouloudefakraoui1@gmail.com>
2025-03-14 12:19:12 +0100

    app terminer

---
+++ TP8_chaine/method/ex3.java
@@ -0,0 +1,7 @@
+public class ex3 {
+    public static void main(String[] args) {
+        int n = method.lectureN();
+        int somme = method.calculeSomme(n);
+        System.out.println("La somme des " + n + " premiers carrés est : " + somme);
+    }
+}
--- TP8_chaine/method/method.java
+++ TP8_chaine/method/method.java
@@ -34,6 +34,13 @@ public class method {
         }
         return co;
     }
+    public static int calculeSomme(int n) {
+        int somme = 0;
+        for (int i = 1; i <= n; i++) {
+            somme += i * i; 
+        }
+        return somme;
+    }
     
     public static void main(String[] args) {
         int[] entiers = new int[10]; 
