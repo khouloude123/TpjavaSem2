6d7f5f989dce2e29549e1ade6b74b26f4da5bf39
khouloude123 <khouloudefakraoui1@gmail.com>
2025-03-14 11:44:17 +0100

    app termine

--- TP8_chaine/method/method.java
+++ TP8_chaine/method/method.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 
 public class method {
     
-    // Méthode pour lire un entier strictement positif
     public static int lectureN() {
         Scanner scanner = new Scanner(System.in);
         int n;
 
         do {
             System.out.print("Entrez un entier strictement positif : ");
-            while (!scanner.hasNextInt()) { // Vérifie que l’entrée est bien un entier
+            while (!scanner.hasNextInt()) { 
                 System.out.println("Erreur : Veuillez entrer un entier valide.");
-                scanner.next(); // Consomme l’entrée invalide
+                scanner.next();
             }
             n = scanner.nextInt();
 
@@ -23,25 +22,26 @@ public class method {
         return n;
     }
 
-    // Méthode pour vérifier si un nombre est pair
+    
     public static boolean EstPair(int n) {
         return n % 2 == 0;
     }
     public static int Compter(int n) {
-        return String.valueOf(n).length();
+        int co = 1; 
+        while (n / 10 != 0) { 
+            n = n / 10; 
+            co++; 
+        }
+        return co;
     }
-
-    // Méthode principale
+    
     public static void main(String[] args) {
-        int[] entiers = new int[10]; // Tableau pour stocker les 10 entiers
+        int[] entiers = new int[10]; 
 
-        // Lecture des 10 entiers
         System.out.println("Veuillez entrer 10 entiers strictement positifs :");
         for (int i = 0; i < 10; i++) {
             entiers[i] = lectureN();
         }
-
-        // Affichage des entiers pairs
         System.out.println("\nLes entiers pairs sont : ");
         for (int n : entiers) {
             if (EstPair(n)) {
